130b6b2497ae2daadfd1eea7bea743ddf5cf7ae5
njsaugat <njsaugat@gmail.com>
2023-07-16 16:53:24 +0545

    react basics covered

---
+++ App.js
@@ -0,0 +1,26 @@
+const heading1 = React.createElement(
+    "h1",
+    {
+      id: "Heading1",
+      key: "heading1",
+    },
+    `Heading 1`
+  );
+  const heading2 = React.createElement(
+    "h1",
+    {
+      id: "Heading2",
+      key: "heading2",
+    },
+    `Heading 2`
+  );
+  const container = React.createElement(
+    "div",
+    {
+      id: "container",
+    },
+    [heading1, heading2]
+  );
+  console.log(container);
+  const root = ReactDOM.createRoot(document.getElementById("root"));
+  root.render(container);
